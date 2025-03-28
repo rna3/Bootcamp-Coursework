cd4e53cba824e4039007c236b9a2840a428de0bc
rna3 <rnalcaraz3@gmail.com>
2025-03-28 14:02:17 -0700

    added express messagely to coursework

---
+++ express-messagely/routes/messages.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const router = new express.Router();
+
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+router.get("/:id", ensureLoggedIn, async (req, res, next) => {
+  try {
+    const message = await Message.get(req.params.id);
+
+    // Only the sender or recipient can view the message
+    if (
+      req.user.username !== message.from_user.username &&
+      req.user.username !== message.to_user.username
+    ) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+router.post("/", ensureLoggedIn, async (req, res, next) => {
+  try {
+    const { to_username, body } = req.body;
+    const from_username = req.user.username;
+
+    const message = await Message.create({ from_username, to_username, body });
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+router.post("/:id/read", ensureLoggedIn, async (req, res, next) => {
+  try {
+    const message = await Message.get(req.params.id);
+
+    // Only the recipient can mark as read
+    if (req.user.username !== message.to_user.username) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+
+    const updatedMessage = await Message.markRead(req.params.id);
+    return res.json({ message: updatedMessage });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
